d09fa310b6fada07ceb405bc59c48e7c83637445
naywilkins512 <nathanwilkins01@gmail.com>
2020-09-26 17:31:49 -0500

    adds inquirer boiler plate

--- Develop/app.js
+++ Develop/app.js
@@ -14,6 +14,75 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+// function to initialize program
+function init() {
+  
+    inquirer
+        .prompt([
+            {
+                message: 'Enter your name',
+                name: 'name',
+            },
+            {
+                message: 'Enter your email',
+                name: 'email',
+            },
+            {
+                message: 'Enter your Github Username',
+                name: 'username',
+            },
+            {
+                message: 'Enter your project Title',
+                name: 'title',
+            },
+            {
+                message: 'Give a brief description',
+                name: 'description',
+            },
+            {
+                message: 'What are the installation instructions?',
+                name: 'install',
+            },
+            {
+            message: 'Enter usage Information',
+            name: 'usage',
+        },
+        {
+            message: 'Enter the Contributing Guidelines (leave blank for default)',
+            name: 'contribution',
+        },
+        {
+        message: 'test instructions',
+        name: 'test',
+        },
+        {
+            type: "list",
+            message: "choose your license",
+            choices: ["MIT license", "GNU General Public License v3.0", "Apache", "The Unlicense"] ,
+            name: 'license',
+        }
+
+        ])
+        .then(answers => {
+            
+            let doc = render(answers)
+
+
+            fs.writeFile('../README.md', doc, function(err){
+                if (err) {
+                    return console.log(err);
+                } else {
+                    console.log("README Generated!")
+                }
+            })
+        });
+
+    }
+
+// // function call to initialize program
+init();
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
